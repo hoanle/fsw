21d03b72c491a76364a901becb35887808a8b483
Hoan Le <hoanle@xtaypro.com>
2020-05-13 14:37:47 +0700

    guessing game

---
+++ w2/wed/script.js
@@ -0,0 +1,49 @@
+let guessMax = 7;
+let guessCount = 0;
+let hiddenNumber = Math.floor(Math.random() * 100);
+let history = "";
+
+let input = document.getElementById('input-number');
+let guessResult = document.getElementById('guess-result');
+let guessRemain = document.getElementById('guess-remain');
+let btnGuess = document.getElementById('btn-guess');
+let txHistory = document.getElementById('guess-history');
+
+guessRemain.textContent = "Guess remains: " + guessMax;
+
+function onGuess() {
+    let value = input.value
+    if (isNaN(parseInt(value))) {
+
+    } else {
+        guessCount += 1
+        guessRemain.textContent = "Guess remains: " + (guessMax-guessCount)
+
+        if (value > hiddenNumber) {
+            guessResult.textContent = "Your guess is greater than the number";
+        } else if (value < hiddenNumber) {
+            guessResult.textContent = "Your guess is smaller than the number";
+        } else {
+            guessResult.textContent = "Your guess is correct. Restart game by Reset button";
+            btnGuess.setAttribute('disabled', 'disabled');
+        }
+        history = history + value + "  ";
+        txHistory.textContent = history;
+        
+        if (guessMax - guessCount == 0) {
+            guessResult.textContent = "You have no guess left. The actual number is " + hiddenNumber;
+            btnGuess.setAttribute('disabled', 'disabled');
+        }
+    }
+}
+
+function onReset() {
+    history = "";
+    btnGuess.removeAttribute('disabled');
+    guessCount = 0;
+    hiddenNumber = Math.floor(Math.random() * 100);
+    input.value = ""
+    txHistory.textContent = "";
+    guessResult.textContent = "Result: ";
+    guessRemain.textContent = "Guess remains: " + guessMax;
+}
