2ad9b8908689b5335385713781e6202328d7f8f8
Hoan Le <hoanle@xtaypro.com>
2020-06-10 10:36:45 +0700

    useRef for number

--- w6/counter-redux/src/components/Counter/Counter.tsx
+++ w6/counter-redux/src/components/Counter/Counter.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, RefObject } from 'react';
 import './Counter.css';
 import { useSelector, useDispatch } from "react-redux";
 import ColorfulTextInput from '../ColorfulTextInput';
 
 const Counter = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
     const count = useSelector((state: any) => state.counterReducer.count);
     const color = useSelector((state: any) => state.colorReducer.color);
     const [step, setStep] = useState(1)
@@ -18,9 +19,11 @@ const Counter = () => {
     }
 
     const onNumberChange = (e:any) => {
-        let number = parseInt(e.target.value); 
-        setStep(number)
+        if (inputRef.current != null && inputRef.current.value != null)  {
+            setStep(parseInt(inputRef.current.value));
+        }
     }
+    
     return (
         <div className="App">
             <h1>{count}</h1>
@@ -28,7 +31,7 @@ const Counter = () => {
             <button onClick={() => dispatch({type: "DECREMENT", step: step})}>Decrement</button>
             <button onClick={() => dispatch({type: "RESET"})}>Reset</button>
             <input onChange={onColorChange} placeholder="color change"></input>
-            <input onChange={onNumberChange} placeholder="number enter" type="number"></input>
+            <input ref={inputRef} onChange={onNumberChange} placeholder="number enter" type="number"></input>
             <div className="Counter-div">
                 {
                     [...Array(count)].map(x => {
